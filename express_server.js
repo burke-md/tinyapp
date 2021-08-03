e703cba421036a36be87b864f3b898bc315c64e9
Michael <michael.dburke56@gmail.com>
2021-08-03 19:20:12 -0400

    Registration feature implimented. Bugs to work out

--- express_server.js
+++ express_server.js
@@ -16,6 +16,20 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+//// users object
+const users = {
+
+  "userRandomID": {
+    id: "1", 
+    email: "1@e1", 
+    password: "1"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 
 //// update existing post db
 app.post('/urls/:id', (req, res) => {
@@ -52,7 +66,7 @@ app.post('/logout', (req, res) => {
 //// display newurl
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"]
+    user: users["username"]
   }
   res.render("urls_new", templateVars);
 });
@@ -72,7 +86,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   const templateVars = {
-    username: req.cookies["username"],
+    user: users["username"],
     shortURL,
     longURL,
   };
@@ -81,12 +95,46 @@ app.get("/urls/:shortURL", (req, res) => {
 
 //// show existing urls
 app.get("/urls", (req, res) => {
+  const userId = req.cookies["username"]
   const templateVars = {
-    username: req.cookies["username"],
+    user: users[userId],
      urls: urlDatabase 
     };
+
+    console.log("\n templateVars.user: \n", templateVars.user)
+
   res.render("urls_index", templateVars);
 });
+
+//// register page
+app.get("/register", (req, res) => {
+  const templateVars = {
+    user: users["username"]
+  }
+  res.render("register", templateVars)
+})
+
+
+//// endpoint for new register
+app.post("/register", (req, res) => {
+
+  const userId = generateRandomString();
+  const userEmail = req.body.email;
+  const userPassword = req.body.password;
+
+  users[userId] = {
+    id: userId,
+    email: userEmail,
+    password: userPassword
+  }
+  res.cookie("username", userId)
+
+  console.log("\nupdates user db: \n", users[userId])
+
+  res.redirect(`/urls`);
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`Test app. Listening on port ${PORT}.`);
